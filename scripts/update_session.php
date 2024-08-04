58e554ee1597b4d224dcff406d142cbf99592e9f
inj-krish19 <kglivee19@gmail.com>
2024-08-04 08:47:39 +0530

    Adding 401 Error Page

--- scripts/update_session.php
+++ scripts/update_session.php
@@ -1,26 +1,20 @@
 <?php
 session_start();
 if( (!isset($_SESSION["user"])) || $_SESSION["user"] == "guest" ){
-    // $_SESSION["user"] = 1;
 }
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['res_id']) && isset($_POST['res_level'])) {
-        // Sanitize the input
         $res_id = filter_var($_POST['res_id'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $res_level = filter_var($_POST['res_level'], FILTER_SANITIZE_NUMBER_INT);
         
-        // Update session variables
         $_SESSION['udV'] = $res_id;
         $_SESSION['udQ'] = $res_level;
 
-        // Send a success response
         echo json_encode(["status" => "success"]);
     } else {
-        // Send an error response if the required parameters are missing
         echo json_encode(["status" => "error", "message" => "Invalid parameters"]);
     }
 } else {
-    // Send an error response if the request method is not POST
     echo json_encode(["status" => "error", "message" => "Invalid request method"]);
 }
 echo "<script> setTimeout(() => { window.location.href = '../scripts/product.php'; }, 3000);  </script>";
