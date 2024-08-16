8f5c6fdd032a91872e8a36672d2373dd1899b003
inj-krish19 <kglivee19@gmail.com>
2024-08-16 08:10:48 +0530

    Modifying Utility Pages

--- scripts/connection.php
+++ scripts/connection.php
@@ -8,20 +8,13 @@
     <link
     href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900,900italic%7cMontserrat:400,700%7cOxygen:400,300,700'
     rel='stylesheet' type='text/css'>
-  <!-- include the site stylesheet -->
   <link rel="stylesheet" href="../css/bootstrap.css">
-  <!-- include the site stylesheet -->
   <link rel="stylesheet" href="../css/animate.css">
-  <!-- include the site stylesheet -->
   <link rel="stylesheet" href="../css/icon-fonts.css">
-  <!-- include the site stylesheet -->
   <link rel="stylesheet" href="../css/main.css">
-  <!-- include the site stylesheet -->
   <link rel="stylesheet" href="../css/responsive.css">
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 </head>
 <body>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>   -->
 </body>
 </html>
 <?php
